35b209767ff274bf4056a1a6aced82c648e059b9
Quốc Trần <tranaiquoc4499@gmail.com>
2024-09-16 22:37:53 +0700

    Build: ini source

---
+++ README.md
@@ -0,0 +1,10 @@
+### Next feature
+- [x] Base telegram bot
+- [ ] Clear message when choose action
+- [ ] Make history list and history action
+- [ ] Make favourite list and favourite action
+- [ ] Make cralwer at 12:00
+- [ ] Make notification recomment films
+- [ ] Admin setting
+- [ ] Change ajax for webapp
+- [ ] Make multi bot and logic add multibot
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,9 @@ module.exports = {
       script: "index.js", // script path to pm2 start
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: 3, // number process of application
+      instances: 5, // number process of application
       autorestart: true, //auto restart if app crashes
-      watch: false,
+      watch: true,
       exec_mode: "cluster",
       max_memory_restart: "1G", // restart if it exceeds the amount of memory specified
     },
